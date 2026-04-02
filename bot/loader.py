998ff21f70e3d3e09730b8fd0a8a2e08714faca7
Makar <poveok177@gmail.com>
2026-04-03 01:36:32 +0300

    ddd

--- bot/loader.py
+++ bot/loader.py
@@ -1,6 +1,12 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties  # Добавили этот импорт
 from aiogram.enums import ParseMode
 from config.config import settings
 
-bot = Bot(token=settings.BOT_TOKEN, parse_mode=ParseMode.HTML)
+# Инициализируем бота по новому стандарту
+bot = Bot(
+    token=settings.BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
 dp = Dispatcher()
